b2d4e5af5b995d9f6dc66872c33edcbae1dffc5c
Anishabaral <130526545+Anishabaral@users.noreply.github.com>
2024-12-25 18:21:58 +0530

    My portfolio

---
+++ index.js
@@ -0,0 +1,16 @@
+// Smooth scrolling for navigation links
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = link.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+  });
+});
